8bcce210ea70b756417998a84d91724fc3d7095e
louhugh <1305525581@qq.com>
2016-03-01 15:30:22 +0800

    第一次提交

---
+++ app/src/main/java/demo01/medsci/cn/demo01/MainActivity.java
@@ -0,0 +1,13 @@
+package demo01.medsci.cn.demo01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
